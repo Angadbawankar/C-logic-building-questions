e5cc831aa41fd6011fd73c4b99bb9730f90a3282
Angad <bawankarangad06@gmail.com>
2025-07-31 11:46:32 +0530

    Did program 2 of day 1(3 to go).

---
+++ num.positive.negative.zero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    printf("Enter your number : ");
+    int number;
+    scanf("%d",&number);
+    
+    if (number<0){
+        printf("Your number is negative.\n");
+    }else if(number==0){
+        printf("your number is zero.\n");
+    }else if(number>0){
+        printf("Your numbe ris positive.\n");
+    }
+    return 0;
+}   
